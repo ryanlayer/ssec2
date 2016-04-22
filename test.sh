e6149824dffba8b3665734b771b6e16577902f9b
Ryan Layer <ryan.layer@gmail.com>
2016-04-22 15:43:57 -0600

    remove debug

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 HOST_NAME=$1
 USER_NAME=$2
-#KEY_ID=$3
-#ACCESS_KEY=$4
-#REGION=$5
-#BUCKET=$6
+KEY_ID=$3
+ACCESS_KEY=$4
+REGION=$5
+BUCKET=$6
 
 echo $HOST_NAME
 echo $USER_NAME
@@ -13,9 +13,9 @@ cause_an_error
 date > test.sh.log
 echo "test run" >> test.sh.log
 
-#AWS_ACCESS_KEY_ID=$KEY_ID \
-#AWS_SECRET_ACCESS_KEY=$ACCESS_KEY \
-#aws s3 cp --region $REGION \
-#test.sh.log s3://$BUCKET
+AWS_ACCESS_KEY_ID=$KEY_ID \
+AWS_SECRET_ACCESS_KEY=$ACCESS_KEY \
+aws s3 cp --region $REGION \
+test.sh.log s3://$BUCKET
 
 
